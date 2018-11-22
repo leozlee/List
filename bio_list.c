075b51ea43c3f3e2060425a968ccd80ae04be7df
leo <leozlee@163.com>
2018-11-22 23:43:42 +0800

    添加双向链表的实现

---
+++ bio_list.c
@@ -0,0 +1,24 @@
+#include"bio_list.h"
+
+struct Node;
+typedef struct Node* PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+typedef int ElementType;
+
+
+struct Node
+{
+	ElementType Element;
+	Position Pre;
+	Position Next;
+};
+
+
+List MakeEmpty(List L);
+int IsEmpty(List L);
+int IsLast(Position P, List L);
+Position Find(ElementType X, List L);
+void Delete(ElementType X, List L);
+void Insert(ElementType X, List L, Position P);
+void DeleteList(List L);
