82322c1b3f2a1d89a94dd2234baf508b1faa285e
leo <leozlee@163.com>
2018-12-04 23:02:03 +0800

    添加栈的实现方式

---
+++ stack.h
@@ -0,0 +1,20 @@
+#ifndef __STACK_H_
+#define __STACK_H_
+
+struct StackRecord;
+typedef struct StackRecord* Stack;
+typedef int ElementType;
+
+
+int IsEmpty(Stack S);
+int IsFull(Stack S);
+Stack CreateStack(int MaxElement);
+void DisposeStack(Stack S);
+void MakeEmpty(Stack S);
+void Push(ElementType x,Stack S);
+ElementType Top(Stack S);
+void Pop(Stack S);
+ElementType TopAndPop(Stack S);
+
+
+#endif
