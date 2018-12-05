40798d696254ad5f1343937e337e3baab92b8c60
leo <leozlee@163.com>
2018-12-05 19:38:59 +0800

    整理代碼

---
+++ stack/stack.h
@@ -0,0 +1,21 @@
+#ifndef _STACK_H__
+#define _STACK_H__
+
+
+struct Stack;
+typedef int ElementType;
+
+
+struct Stack
+{
+	ElementType *base;
+	ElementType *top;
+	int stacksize;
+};
+
+int InitStack(
+
+
+
+
+#endif
