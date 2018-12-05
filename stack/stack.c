ee039ca568f0d8d70beebe83fc2a59cd2e16f991
leo <leozlee@163.com>
2018-12-05 22:53:06 +0800

    栈的实现

---
+++ stack/stack.c
@@ -0,0 +1,128 @@
+#include "stack.h"
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+
+//栈的数组实现
+
+#define EmptyTOS (-1)
+#define MinStackSize (5)
+
+struct StackRecord
+{
+	int Capacity;
+	int TopOfStack;
+	ElementType *Array;
+};
+
+
+
+//创建一个栈
+Stack CreateStack(int MaxElements)
+{
+	Stack S;
+
+	if(MaxElements < MinStackSize)
+	{
+		printf("Stack size is too small\r\n");
+	}
+	
+	S = (Stack)malloc(sizeof(struct StackRecord));
+	if(S == NULL)
+	{
+		perror("out of space");
+		return NULL;
+	}
+		
+
+	S->Array = malloc(sizeof(ElementType) * MaxElements);
+	if(S->Array == NULL)
+	{
+		printf("Out of space");
+		return NULL;
+	}
+	S->Capacity = MaxElements;
+	MakeEmpty(S);
+
+	return S;
+}
+
+
+//销毁栈的函数
+void DispostStack(Stack S)
+{
+	if(S != NULL)
+	{
+		free(S->Array);
+		free(S);
+	}
+}
+
+
+
+// 判断栈是否为空
+int IsEmpty(Stack S)
+{
+	return S->TopOfStack == EmptyTOS;
+}
+
+
+//创建一个空栈
+void MakeEmtpy(Stack S)
+{
+	S->TopOfStack = EmptyTOS;
+}
+
+
+
+void Push(ElementType X, Stack S)
+{
+	if(IsFull(S))
+	{
+		printf("Full Stack\r\n");
+		return -1;
+	}
+	else
+	{
+		S->Array[++S->TopOfStack] = X;
+	}
+}
+
+ElementType Top(Stack S)
+{
+	if(!IsEmpty(S))
+	{
+		return S->Array[S->TopOfStack];
+	}
+	printf("Empty Stack\r\n");
+	return 0;
+}
+
+
+void Pop(Stack S)
+{
+	if(IsEmpty(s))
+	{
+		printf("Empty Stack\r\n");
+	}
+	else
+	{
+		S->TopOfStack--;
+	}
+}
+
+
+ElementType TopAndPop(Stack S)
+{
+	if(!IsEmpty(S))
+	{
+		return S->Array[S->TopOfStack--];
+	}
+	printf("Empty Stack\r\n");
+	return 0;
+	
+	
+
+}
+
--- stack/stack.h
+++ stack/stack.h
@@ -1,21 +1,20 @@
-#ifndef _STACK_H__
-#define _STACK_H__
+#ifndef __STACK_H_
+#define __STACK_H_
 
-
-struct Stack;
+struct StackRecord;
+typedef struct StackRecord* Stack;
 typedef int ElementType;
 
 
-struct Stack
-{
-	ElementType *base;
-	ElementType *top;
-	int stacksize;
-};
-
-int InitStack(
-
-
+int IsEmpty(Stack S);
+int IsFull(Stack S);
+Stack CreateStack(int MaxElement);
+void DisposeStack(Stack S);
+void MakeEmpty(Stack S);
+void Push(ElementType x,Stack S);
+ElementType Top(Stack S);
+void Pop(Stack S);
+ElementType TopAndPop(Stack S);
 
 
 #endif
